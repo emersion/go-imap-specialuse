f785d48dfb694cc13946dd8879c07d9d0d97d6b8
emersion <contact@emersion.fr>
2016-08-11 15:43:57 +0200

    Adds compatibility with latest go-imap

--- server.go
+++ server.go
@@ -1,12 +1,26 @@
 package specialuse
 
 import (
-	"github.com/emersion/go-imap/common"
+	"github.com/emersion/go-imap"
 	"github.com/emersion/go-imap/server"
 )
 
-// Enable this extension on a server. The backend must support this extension
-// too.
-func NewServer(s *server.Server) {
-	s.RegisterCapability(Capability, common.AuthenticatedState)
+type extension struct {}
+
+func (ext *extension) Capabilities(state imap.ConnState) []string {
+	var caps []string
+
+	if state & imap.AuthenticatedState != 0 {
+		caps = append(caps, Capability)
+	}
+
+	return caps
+}
+
+func (ext *extension) Command(name string) server.HandlerFactory {
+	return nil
+}
+
+func NewExtension() server.Extension {
+	return &extension{}
 }
