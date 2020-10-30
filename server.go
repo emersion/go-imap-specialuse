d9555a225f43f2d247482fe6a05b1efcf0df6869
Radomír Polách <rp@t4d.cz>
2020-10-30 11:45:38 +0100

    change: remove SPECIAL-USE announcement

--- server.go
+++ server.go
@@ -1,16 +1,12 @@
 package specialuse
 
 import (
-	"github.com/emersion/go-imap"
 	"github.com/emersion/go-imap/server"
 )
 
 type extension struct{}
 
 func (ext *extension) Capabilities(c server.Conn) []string {
-	if c.Context().State&imap.AuthenticatedState != 0 {
-		return []string{Capability, ImportantCapability}
-	}
 	return nil
 }
 
